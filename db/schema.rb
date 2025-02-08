64fada5a4564be120916cb54dc77bd37e3d5f3fd
benxta4424 <benxtaben@gmail.com>
2025-02-08 04:08:30 +0200

    addign a photos model for the users (ids match a photo for each user)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_06_012542) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_08_020522) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -23,6 +23,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_06_012542) do
     t.string "content"
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_photos_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
