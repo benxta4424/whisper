3c767d5aff5888b400d23183d87844499e8fe898
benxta4424 <benxtaben@gmail.com>
2025-02-10 17:08:07 +0200

    creating the posts controller responsible for the post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,29 @@
 class PostsController < ApplicationController
   def new
+    @post=Post.new
   end
 
   def index
+    @posts=Post.all
   end
 
   def create
+    @post=current_user.post.build(post_params)
+    if @post.save
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to root_path, notice: "Post Created!" }
+      end
+    else
+      render "pages/index", status: :unprocessable_entity    end
   end
 
   def show
+    @post=Post.find(params[:id])
+  end
+
+  private
+  def post_params
+    params.require(:posts).permit(:title, :body)
   end
 end
