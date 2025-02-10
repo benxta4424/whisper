0b9aff8cfc04fdc5e3d81a7412430e841f317119
benxta4424 <benxtaben@gmail.com>
2025-02-10 16:02:20 +0200

    i wanted to generate a scaffold for the posts but made a mistake and fixed it in the end

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  root "pages#index"
+  resources :controllers
   resources :pages, only: [ :index ]
 
   devise_for :users
   resources :users, only: [ :show ] do
     resources :friendships, only: [ :new, :create, :show, :destroy ]
+    resources :posts
   end
 
 
