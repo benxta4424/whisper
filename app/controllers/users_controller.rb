41036a80f86ea0beb9838a74ccb51c7bdfc44136
benxta4424 <benxtaben@gmail.com>
2025-02-15 16:52:07 +0200

    adding slight modifications for the lazy load (still didnt work tho..)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
     @user=User.find(params[:id])
     @followers=@user.followers
     @followed=@user.following
+    @posts = @user.posts.order(created_at: :desc)
   end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,6 @@
-# Pin npm packages by running ./bin/importmap
-
-pin "application"
+# config/importmap.rb
+pin "application", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
