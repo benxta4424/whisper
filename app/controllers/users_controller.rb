ec9b6245641ef327781d3678785a4e92769d80ad
benxta4424 <benxtaben@gmail.com>
2025-03-03 03:09:10 +0200

    adding sugestions when searching for users (limit of 10 alphabetical order)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.order(:name)
+    @users = @users.where("name like ?", "%#{params[:term]}%") if params[:term]
+
+    respond_to do |format|
+      format.html  # index.html.erb
+      format.json  { render json: @users.map(&:name) }
+    end
+  end
   def show
     @user=User.find(params[:id])
     @followers=@user.followers
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,8 +1,9 @@
 # config/importmap.rb
 pin "application", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @8.0.12
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.12
 pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @7.2.201
+pin "stimulus-autocomplete" # @3.1.0
