99e9bab1306a73171b4bc94f1829a67eaddff64c
benxta4424 <benxtaben@gmail.com>
2025-03-09 00:18:12 +0200

    creating the friendship controller and its 2 methods for the following/unfollowing logic

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,28 @@
 class FriendshipsController < ApplicationController
+  def create
+    @friendship=Friendship.create(friendship_params)
+
+    respond_to do |format|
+      if @friendship.save
+        format.turbo_stream
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @find_friendship=Friendship.where(follower_id: current_user.id, followed_id: @user.id)
+    if @find_friendship
+      @find_friendship&.destroy
+    else
+      flash[:friendship_not_found]="Friendship not found!"
+    end
+  end
+
+  private
+
+  def friendship_params
+    params.require(:friendship).permit(:follower_id, :followed_id)
+  end
 end
