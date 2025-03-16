b199650ffdaddfabdf7735a766b16624fee93d53
benxta4424 <benxtaben@gmail.com>
2025-03-16 02:24:55 +0200

    adding the messages model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_05_230117) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_16_002143) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -68,6 +68,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_05_230117) do
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_likes_on_post_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "photos", force: :cascade do |t|
