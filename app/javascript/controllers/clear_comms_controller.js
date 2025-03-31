e431944477784ae4cc099e7415a20dc9bc95b82a
benxta4424 <benxtaben@gmail.com>
2025-03-31 17:45:03 +0300

    adding a simpler reset (i got a binding error on the clear method)

--- app/javascript/controllers/clear_comms_controller.js
+++ app/javascript/controllers/clear_comms_controller.js
@@ -2,10 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="clear-comms"
 export default class extends Controller {
-  connect() {
-    this.element.addEventListener("submit", clear);
-  }
-
   clear() {
     this.element.reset()
   }
